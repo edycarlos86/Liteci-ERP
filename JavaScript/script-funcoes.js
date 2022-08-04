3ecded5f9e0d013aa7b0ff958b5f30fe4ec54491
Edy Carlos <105792486+edycarlos86@users.noreply.github.com>
2022-08-04 13:56:57 -0300

    Update script-funcoes.js

--- JavaScript/script-funcoes.js
+++ JavaScript/script-funcoes.js
@@ -23,9 +23,9 @@ function atualizarSelect() {
       vdivpesquisa = get_div_pesquisa_periodo
       $('#div_pesquisa').append(vdivpesquisa);
       
-      $(document).ready(function() {
+      /*$(document).ready(function() {
       $('#div_lista').append('<div id="div_lista" class="container d-flex flex-wrap justify-content-center">'+vdiv+'</div>');
-    });
+    });*/
 
       for (let index = 0; index < 4; index++) {
         vdiv = get_div_quitadas('02/05/2022', 50, '15/04/2022', 100, 215458, 100, 'PIX', 5421, 2564, 'TOMATE KG', 2, 4, 8, '25/05/2022', '14:15:52', 0, 0, 100);
